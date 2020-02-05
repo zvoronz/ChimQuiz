f1a743f79b1fb10698ba9cb8ed41ba1092da10ec
zvoronz <voron.zvs@gmail.com>
2020-02-06 00:11:53 +0200

    + ChimQuiz Cordova initial commit

---
+++ www/precache-manifest.8a98e9a471d2efbc436c808dab72171d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c441b11d434ebcb5bdefe05e3cb946",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87f8a8e5ccbeb5be1b36",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1857a2f1281e34f40443",
+    "url": "./static/css/main.7a78d67d.chunk.css"
+  },
+  {
+    "revision": "87f8a8e5ccbeb5be1b36",
+    "url": "./static/js/2.e1ee5667.chunk.js"
+  },
+  {
+    "revision": "5728c8a1c0470673b334ef65999766ad",
+    "url": "./static/js/2.e1ee5667.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1857a2f1281e34f40443",
+    "url": "./static/js/main.952bbab2.chunk.js"
+  },
+  {
+    "revision": "3d81c6c870343073a8d3",
+    "url": "./static/js/runtime-main.b47e0cfd.js"
+  }
+]);
